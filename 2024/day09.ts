5025b051774933b4c645709f8a2ea79f261af949
Mikko Heikelä <mikko.heikela@gmail.com>
2024-12-14 16:31:53 +0000

    Day 9 initial solution to part 2

--- 2024/day09.ts
+++ 2024/day09.ts
@@ -7,6 +7,48 @@ import { Map, Record, RecordOf } from "immutable";
 const input = await getInput(2024, 9);
 //const input = "2333133121414131402";
 
+/*
+type span = {
+  len: number;
+  id: number;
+};
+
+let spans: span[] = [];
+for (let i = 0; i < input.length; i++) {
+  const len = parseInt(input[i]);
+  const id = i % 2 == 0 ? i / 2 : -1;
+  spans.push({ len, id });
+}
+
+let compactedSpans: span[] = [];
+let toMove = spans.length - 1;
+while (spans[toMove].id == -1) {
+  toMove--;
+}
+
+while (toMove > 0) {
+  const spanToMove = spans[toMove];
+  let emptyIndex = 0;
+  while ((emptyIndex < toMove && spans[emptyIndex].id != -1) || spans[emptyIndex].len < spanToMove.len) {
+    emptyIndex++;
+  }
+  if (emptyIndex == toMove) {
+    toMove--;
+    continue
+  }
+  const emptySpan = spans[emptyIndex];
+  spans[emptyIndex] = spanToMove;
+  expanded[toMove] = destination;
+  while (expanded[empty] != -1) {
+    empty++;
+  }
+  while (expanded[toMove] == -1) {
+    toMove--;
+  }
+  //  console.log(`empty: ${empty}, toMove: ${toMove}`);
+}
+  */
+
 let expanded: number[] = [];
 for (let i = 0; i < input.length; i++) {
   const len = parseInt(input[i]);
@@ -21,34 +63,57 @@ for (let i = 0; i < input.length; i++) {
 
 //console.log("expanded");
 
-let empty = 0;
-let toMove = expanded.length - 1;
-while (expanded[toMove] == -1) {
-  toMove--;
+let toMoveEnd = expanded.length - 1;
+while (expanded[toMoveEnd] == -1) {
+  toMoveEnd--;
 }
 
-while (expanded[empty] != -1) {
-  empty++;
-}
-
-while (toMove > empty) {
-  const value = expanded[toMove];
-  const destination = expanded[empty];
-  expanded[empty] = value;
-  expanded[toMove] = destination;
-  while (expanded[empty] != -1) {
-    empty++;
+while (toMoveEnd > 0) {
+  //  console.log("toMoveEnd", toMoveEnd);
+  const value = expanded[toMoveEnd];
+  let toMoveStart = toMoveEnd;
+  while (toMoveStart > 0 && expanded[toMoveStart - 1] == value) {
+    toMoveStart--;
   }
-  while (expanded[toMove] == -1) {
-    toMove--;
+  const len = toMoveEnd - toMoveStart + 1;
+  let emptyStart = 0;
+  let found = false;
+  while (emptyStart < toMoveStart && !found) {
+    if (expanded[emptyStart] == -1) {
+      let tooShort = false;
+      for (let i = 0; i < len; i++) {
+        if (expanded[emptyStart + i] != -1) {
+          tooShort = true;
+          break;
+        }
+      }
+      if (!tooShort) {
+        found = true;
+      }
+    }
+    if (!found) {
+      emptyStart++;
+    }
   }
-  //  console.log(`empty: ${empty}, toMove: ${toMove}`);
+  if (found) {
+    for (let i = 0; i < len; i++) {
+      expanded[emptyStart + i] = value;
+      expanded[toMoveStart + i] = -1;
+    }
+  }
+  toMoveEnd = toMoveStart - 1;
+  while (expanded[toMoveEnd] == -1) {
+    toMoveEnd--;
+  }
 }
 
 //console.log("compacted", expanded);
 
 let checksum = 0;
-for (let i = 0; i <= toMove; i++) {
+for (let i = 0; i < expanded.length; i++) {
+  if (expanded[i] == -1) {
+    continue;
+  }
   checksum += expanded[i] * i;
 }
 
